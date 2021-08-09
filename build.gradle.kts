d267ba1058b435b489f8b9274c7c264f8963345d
Michael Friend <michaelf@touchlab.co>
2021-08-09 14:56:13 -0400

    new sqldelight

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,8 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:7.0.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
-        classpath("com.squareup.sqldelight:gradle-plugin:1.4.4")
+//        classpath("com.squareup.sqldelight:gradle-plugin:1.4.4")
+        classpath("com.squareup.sqldelight:gradle-plugin:1.5.1")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
